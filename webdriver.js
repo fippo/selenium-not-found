862ed856e25652ed29a057937dd8ee71951117f4
Philipp Hancke <philipp.hancke@googlemail.com>
2023-08-24 14:03:17 +0200

    try absolute path (and one that is not a symlink) and enable logging

--- webdriver.js
+++ webdriver.js
@@ -1,15 +1,24 @@
 const os = require('os');
 
 const webdriver = require('selenium-webdriver');
+const logging = require('selenium-webdriver/lib/logging')
+logger = logging.getLogger('webdriver')
+logger.setLevel(logging.Level.ALL)
+console.log('LOGGER', logger);
+logging.installConsoleHandler()
 const chrome = require('selenium-webdriver/chrome');
 
 const chromeOptions = new chrome.Options();
-chromeOptions.setChromeBinaryPath('browsers/bin/chrome-unstable');
+//chromeOptions.setChromeBinaryPath(process.cwd() + '/browsers/bin/chrome-unstable');
+chromeOptions.setChromeBinaryPath(process.cwd() + '/node_modules/travis-multirunner/browsers/chrome/unstable/chrome');
 const driver = new webdriver.Builder()
   .setChromeOptions(chromeOptions)
+  /*
   .setChromeService(
       new chrome.ServiceBuilder().addArguments('--disable-build-check')
-  ).forBrowser('chrome')
+  )
+  */
+  .forBrowser('chrome')
   .build();
 
   driver.get('chrome://version')
